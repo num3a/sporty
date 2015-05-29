9c2f975cf111fdf378e22159fdc849ef125866e6
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2015-05-29 15:38:24 +0200

    Add mobile config change backend ip address

---
+++ mobile-config.js
@@ -0,0 +1,2 @@
+App.accessRule('http://meteor.local/*');
+App.accessRule('http://178.62.127.71/*');
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,3 +1,4 @@
 Meteor.startup(function() {
     Kadira.connect('gRj2JSHZbaxcL8Ehr', '46c9e030-3d5b-4772-9d1b-5e5426dd052d');
 });
+
